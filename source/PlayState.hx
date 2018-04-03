bb9d2d264a984022626d4fde5e36bfb615157e2a
Sam Bumgardner <sambumgardner1@gmail.com>
2018-04-02 19:11:56 -0500

    in-class-example - Demonstrated basic input.

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,16 +1,26 @@
 package;
 
+import flixel.FlxG;
 import flixel.FlxState;
+import flixel.input.FlxKeyManager;
 
 class PlayState extends FlxState
 {
+	private var player:Player;
+	
 	override public function create():Void
 	{
 		super.create();
+		player = new Player(32, 32);
+		add(player);
 	}
 
 	override public function update(elapsed:Float):Void
 	{
+		if (FlxG.keys.pressed.RIGHT) {
+			player.x += 5; // we want to update player x and / or y postion
+		}
+		
 		super.update(elapsed);
 	}
 }
