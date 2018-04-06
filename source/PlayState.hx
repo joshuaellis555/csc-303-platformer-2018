cf600e6a9db24d202ece253a71d0786046970565
Sam Bumgardner <sambumgardner1@gmail.com>
2018-04-05 19:29:34 -0500

    assignment-3-setup - created files for the different Hero-type classes and added instances of them to the PlayState.

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,16 +1,37 @@
 package;
 
 import flixel.FlxState;
+import player.PlatformerHero;
+import player.TopDownHero;
 
 class PlayState extends FlxState
 {
-	override public function create():Void
-	{
+	private var topDownHero:TopDownHero;
+	private var platformerHero:PlatformerHero;
+	
+	override public function create():Void {
 		super.create();
+		instantiateEntities();
+		addEntities();
+	}
+	
+	/**
+	 * Helper function that generates all starting objects.
+	 */
+	private function instantiateEntities():Void {
+		topDownHero = new TopDownHero(32, 32);
+		platformerHero = new PlatformerHero(300, 200);
+	}
+	
+	/**
+	 * Helper function that adds all starting objects to the Scene.
+	 */
+	private function addEntities():Void {
+		add(topDownHero);
+		add(platformerHero);
 	}
 
-	override public function update(elapsed:Float):Void
-	{
+	override public function update(elapsed:Float):Void {
 		super.update(elapsed);
 	}
 }
