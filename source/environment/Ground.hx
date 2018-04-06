e34219424bca7eb819b56731f348b6bc674db370
Sam Bumgardner <sambumgardner1@gmail.com>
2018-04-05 19:42:55 -0500

    assignment-3-setup - created Ground class. Also, my previous commits probably didn't actually do what I said they did, since I didn't actually add the new files to the commits properly.

---
+++ source/environment/Ground.hx
@@ -0,0 +1,21 @@
+package environment;
+
+import flixel.FlxSprite;
+import flixel.system.FlxAssets.FlxGraphicAsset;
+import flixel.util.FlxColor;
+
+/**
+ * Simple sprite used for ground and / or walls for the PlatformerHero.
+ * @author Samuel Bumgardner
+ */
+class Ground extends FlxSprite
+{
+	public static var LENGTH(default, never) = 32;
+	public static var HEIGHT(default, never) = 32;
+
+	public function new(?X:Float=0, ?Y:Float=0) {
+		super(X, Y);
+		makeGraphic(LENGTH, HEIGHT, FlxColor.GRAY);
+	}
+	
+}
