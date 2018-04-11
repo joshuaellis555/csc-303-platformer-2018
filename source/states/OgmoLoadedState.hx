c99831ba887f662465a3255c30314c716426b357
Sam Bumgardner <sambumgardner1@gmail.com>
2018-04-11 18:54:11 -0500

    finished ogmoloadedstate stuff.

--- source/states/OgmoLoadedState.hx
+++ source/states/OgmoLoadedState.hx
@@ -1,6 +1,11 @@
 package states;
 
+import flixel.FlxG;
+import flixel.FlxObject;
 import flixel.FlxState;
+import flixel.addons.editors.ogmo.FlxOgmoLoader;
+import flixel.tile.FlxTilemap;
+import player.Player;
 
 /**
  * ...
@@ -8,10 +13,55 @@ import flixel.FlxState;
  */
 class OgmoLoadedState extends FlxState
 {
-
-	public function new() 
-	{
-		
+	private var player:Player;
+	private var levelLoader:FlxOgmoLoader;
+	private var map:FlxTilemap;
+	
+	override public function create():Void {
+		super.create();
+		instantiateEntities();
+		setUpLevel();
+		addAll();
 	}
 	
+	/**
+	 * Helper function that generates all starting objects.
+	 */
+	private function instantiateEntities():Void {
+		player = new Player();
+	}
+	
+	/**
+	 * 
+	 */
+	private function setUpLevel():Void {
+		levelLoader = new FlxOgmoLoader(AssetPaths.brick_land__oel);
+		
+		map = levelLoader.loadTilemap(AssetPaths.Brick__png, 32, 32, "solid_obstacles");
+		map.setTileProperties(1, FlxObject.ANY);
+		
+		levelLoader.loadEntities(placeEntities, "entities");
+	}
+	
+	private function placeEntities(entityName:String, entityData:Xml):Void {
+		var x:Int = Std.parseInt(entityData.get("x"));
+		var y:Int = Std.parseInt(entityData.get("y"));
+		if (entityName == "player") {
+			player.x = x;
+			player.y = y;
+		}
+	}
+	
+	/**
+	 * Helper function that adds all starting objects to the Scene.
+	 */
+	private function addAll():Void {
+		add(player);
+		add(map);
+	}
+
+	override public function update(elapsed:Float):Void {
+		super.update(elapsed);
+		FlxG.collide(map, player);
+	}
 }
